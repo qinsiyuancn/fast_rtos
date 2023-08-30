d478de22be6da1a2283f0c2f4e6cb480c5d2a71b
qinsiyuan <130231069@qq.com>
2023-08-30 17:09:22 +0800

    redesign i2c of lpc17xx
    Signed-off-by: qinsiyuan <130231069@qq.com>

--- cpu/lpc17xx/i2c_lpc17xx.c
+++ cpu/lpc17xx/i2c_lpc17xx.c
@@ -7,98 +7,128 @@
 #include "i2c_lpc17xx.h"
 
 #if CPU_lpc17xx
-static const struct i2c_s i2cs[] = I2C_A;
+// static const struct i2c_s i2cs[] = I2C_A;
+static struct {
+    const struct i2c_s i2cs;
+    struct i2c_session session;
+} bus[] = I2C_A;
 
-static unsigned int I2CStop( unsigned char fd )   
-{   
-  i2cs[fd].bus->I2CONSET = I2CONSET_STO;  /* Set Stop flag */    
-  i2cs[fd].bus->I2CONCLR = I2CONCLR_SIC;  /* Clear SI flag */    
-               
-  /*--- Wait for STOP detected ---*/   
-//  while( i2cs[fd].bus->I2CONSET & I2CONSET_STO );   
-  return TRUE;   
+static unsigned int I2CStop( unsigned char fd ) 
+{
+    bus[fd].i2cs.bus->I2CONSET = I2CONSET_STO;  /* Set Stop flag */
+    bus[fd].i2cs.bus->I2CONCLR = I2CONCLR_SIC;  /* Clear SI flag */
+    /*--- Wait for STOP detected ---*/
+    //  while( i2cs[fd].bus->I2CONSET & I2CONSET_STO );
+    return TRUE;
 }
 
 static int clear_sic(unsigned char fd)
 {
-	i2cs[fd].bus->I2CONCLR = I2CONCLR_SIC;
-	return 0;
+    bus[fd].i2cs.bus->I2CONCLR = I2CONCLR_SIC;
+    return 0;
 }
 
 static int set_ack(unsigned char fd)
 {
-	i2cs[fd].bus->I2CONSET = I2CONSET_AA;	/* assert ACK after data is received */
-	clear_sic(fd);
-	return 0;
+    bus[fd].i2cs.bus->I2CONSET = I2CONSET_AA;	/* assert ACK after data is received */
+    clear_sic(fd);
+    return 0;
 }
 
 
 static int set_nack(unsigned char fd)
 {
-	i2cs[fd].bus->I2CONCLR = I2CONCLR_SIC |I2CONCLR_AAC;
-	return 0;
+    bus[fd].i2cs.bus->I2CONCLR = I2CONCLR_SIC |I2CONCLR_AAC;
+    return 0;
 }
 
 static int send_sla(unsigned char fd, unsigned char state)
 {
 	/* A Start condition is issued. */
-	i2cs[fd].bus->I2DAT = i2c_base_get_addr(i2c_bus_manager, fd);   
-	i2cs[fd].bus->I2CONCLR = (I2CONCLR_SIC | I2CONCLR_STAC);
-	return 0;   
+    bus[fd].i2cs.bus->I2DAT = i2c_base_get_addr(i2c_bus_manager, fd);
+    bus[fd].i2cs.bus->I2CONCLR = (I2CONCLR_SIC | I2CONCLR_STAC);
+    return 0;
 }
 
-static int stop(unsigned char fd, unsigned char state)
-{
-	i2c_base_transport_finish(i2c_bus_manager, fd);
-//	clear_sic(fd,state);
-	I2CStop(fd);
-	return 0;
-}
 static int send_data(unsigned char fd, unsigned char state)
 {
-	char ch[1] = {0};
-	if(i2c_base_send_char(i2c_bus_manager, fd, ch)){
-//		i2c_base_transport_finish(i2c_bus_manager, fd);
-//		I2CStop(fd);
-		stop(fd, state);
-		return 1;
-	}   
-    i2cs[fd].bus->I2DAT = *ch;
+    char ch[1] = {0};
+    if(i2c_base_send_char(i2c_bus_manager, fd, ch)){
+//    i2c_base_transport_finish(i2c_bus_manager, fd);
+//    I2CStop(fd);
+        stop(fd, state);
+        return 1;
+    }
+    bus[fd].i2cs.bus->I2DAT = *ch;
     clear_sic(fd);
-	return 0;
+    return 0;
 }
 
 static int release_bus(unsigned char fd, unsigned char state)
 {
-	return clear_sic(fd);
+    return clear_sic(fd);
 }
+
 static int recv_ready(unsigned char fd, unsigned char state)
 {
-	if(i2c_base_get_msg_size(i2c_bus_manager, fd) < 2){
-		set_nack(fd);
-	}else{
-		set_ack(fd);	
-	}
-	return 0;
+    if(i2c_base_get_msg_size(i2c_bus_manager, fd) < 2){
+        set_nack(fd);
+    }else{
+        set_ack(fd);
+    }
+    return 0;
 }
 
 static int recv_data(unsigned char fd, unsigned char state)
 {
-	if(i2c_base_recv_char(i2c_bus_manager, fd, i2cs[fd].bus->I2DAT) < 2){
-		set_nack(fd);	
-	}else{
-		set_ack(fd);			
-	}
-	return 0;
+    if (i2c_base_recv_char(i2c_bus_manager, fd, bus[fd]i2cs.bus->I2DAT) < 2) {
+        set_nack(fd);
+    }else{
+        set_ack(fd);
+    }
+    return 0;
 }
 static int recv_last_data(unsigned char fd, unsigned char state)
 {
-	i2c_base_recv_char(i2c_bus_manager, fd, i2cs[fd].bus->I2DAT);
-	stop(fd, state);
-	return 0;	
+    i2c_base_recv_char(i2c_bus_manager, fd, bus[fd]i2cs.bus->I2DAT);
+    stop(fd, state);
+    return 0;
 }
 
-static irq_funp const handler_state[0x20] = {
+static unsigned int start(unsigned char fd)
+{
+    if(fd >= i2c_count())return 1;
+    /*--- Issue a start condition ---*/
+    bus[fd]i2cs.bus->I2CONSET = I2CONSET_STA; /* Set Start flag */
+    /*--- Wait until START transmitted ---*/
+    return 0;
+}
+
+unsigned int i2c_start(unsigned char fd )
+{
+    if(fd >= i2c_count())return 1;
+    bus[fd].session.buffer.buffer = bus[fd].session.buffer.default_buffer;
+    bus[fd].session.buffer.current = 0;
+    bus[fd].session.buffer.size = 0;
+    return start(fd);
+}
+
+unsigned int i2c_stop(unsigned char fd, unsigned char state)
+{
+    // i2c_base_transport_finish(i2c_bus_manager, fd);
+//    clear_sic(fd,state);
+    if(fd < i2c_count())
+        if(!stop(fd)) {
+            bus[fd].session.buffer.buffer = NULL;
+            bus[fd].session.buffer.current = 0;
+            bus[fd].session.buffer.size = 0;
+        }
+    return 1;
+}
+
+void I2C_IRQHandler(unsigned char fd)
+{
+    static irq_funp const handler_state[0x20] = {
         /*0x00 由于非法起始或停止条件的出现，在MST或选择的从机模式中将出现总线错误。			 
             当外部干扰使I2C 模块进入未定义的状态时也出现0x00状态*/  NULL, 	  
         /*0x08 已发送起始条件*/                                     send_sla,  
@@ -112,63 +142,60 @@ static irq_funp const handler_state[0x20] = {
         /*0x48 已发送SLA+R，已接收非ACK*/                           stop,
         /*0x50 已接收数据字节，ACK已返回*/                          recv_data,	
         /*0x58 已接收数据字节，非ACK已返回*/                        recv_last_data,
-};
+    };
 
-static unsigned char i2c_work( unsigned char fd )   
-{    
-  if(fd > i2c_count())return 1;  
-  /*--- Issue a start condition ---*/   
-  i2cs[fd].bus->I2CONSET = I2CONSET_STA; /* Set Start flag */       
-  /*--- Wait until START transmitted ---*/      
-  return 0;   
-}
-
-static void I2C_IRQHandler(unsigned char fd)
-{
-	const unsigned i2c_stat = i2cs[fd].bus->I2STAT;
-	if(handler_state[ i2c_stat >> 3])
-		handler_state[ i2c_stat >> 3 ](fd, i2c_stat);
-	else
-		stop(fd, i2c_stat);
+    const unsigned i2c_stat = bus[fd].i2cs.bus->I2STAT;
+    if(handler_state[ i2c_stat >> 3])
+        handler_state[ i2c_stat >> 3 ](fd, i2c_stat);
+    else
+        stop(fd, i2c_stat);
 }
 
 static void set_slave(unsigned int fd)
 {
-    
-    i2cs[fd].bus->I2ADR0 = (i2c_base_get_addr(i2c_bus_manager, fd) & 0xfe) | (i2cs[fd].bus->I2ADR0 & 1);//只改地址，不改通用使能位   	
-    i2cs[fd].bus->I2CONSET = I2CONSET_I2EN | I2CONSET_AA;
+    bus[fd].i2cs.bus->I2ADR0 = (i2c_base_get_addr(i2c_bus_manager, fd) & 0xfe) | (bus[fd].i2cs.bus->I2ADR0 & 1);//只改地址，不改通用使能位
+    bus[fd].i2cs.bus->I2CONSET = I2CONSET_I2EN | I2CONSET_AA;
 }
 
 static void set_master(unsigned int fd)
 {
-
-    i2cs[fd].bus->I2CONSET = I2CONSET_I2EN;
+    bus[fd].i2cs.bus->I2CONSET = I2CONSET_I2EN;
 }
 
-static int i2c_bus_init(unsigned char fd, enum i2c_mode mode)
-{	
-	static void (* const set_mode[])(unsigned int) = {set_slave, set_master};
-	if(fd >= i2c_count())return 1;
- 
-	LPC_SC->PCONP |= (1 << i2cs[fd].pconp);
+int i2c_bus_init(unsigned char fd)
+{
+    static void (* const set_mode[])(unsigned int) = {set_slave, set_master};
 
-	*(i2cs[fd].pin.pin_p) |= i2cs[fd].pin.pin_v;
+    if(fd >= i2c_count())return 1;
 
-	/*--- Clear flags ---*/   
-	i2cs[fd].bus->I2CONCLR = I2CONCLR_AAC | I2CONCLR_SIC | I2CONCLR_STAC | I2CONCLR_I2ENC;       
-	   
-	/*--- Reset registers ---*/   
-	i2cs[fd].bus->I2SCLL = I2SCLL_SCLL;   
-	i2cs[fd].bus->I2SCLH = I2SCLH_SCLH;   
-          
-	  /* Install interrupt handler */   
-	NVIC_EnableIRQ(I2C0_IRQn + fd);
-	set_mode[mode](fd);
-	return 0;	
+    LPC_SC->PCONP |= (1 << bus[fd].i2cs.pconp);
+
+    *(bus[fd].i2cs.pin.pin_p) |= bus[fd].i2cs.pin.pin_v;
+
+    /*--- Clear flags ---*/
+    bus[fd].i2cs.bus->I2CONCLR = I2CONCLR_AAC | I2CONCLR_SIC | I2CONCLR_STAC | I2CONCLR_I2ENC;
+
+    /*--- Reset registers ---*/
+    bus[fd].i2cs.bus->I2SCLL = I2SCLL_SCLL;
+    bus[fd].i2cs.bus->I2SCLH = I2SCLH_SCLH;
+
+    /* Install interrupt handler */
+    NVIC_EnableIRQ(I2C0_IRQn + fd);
+    set_mode[bus[fd].config.mode](fd);
+    return 0;
 }
 
+int i2c_bus_init_all()
+{
+    unsigned int ret = 0;
+    unsigned char i = 0;
+    while(i < (sizeof(bus)/sizeof(bus[0]))) {
+        ret = ret || i2c_bus_init(i++);
+    }
+    return ret;
+}
 
-static const struct i2c_manager_s i2c_manager = {(sizeof(i2cs)/sizeof(i2cs[0])), i2c_bus_init, i2c_work, I2CStop, I2C_IRQHandler, &i2c_listener};
-const struct i2c_manager_s * const i2c_bus_manager = &i2c_manager;
+// static const struct i2c_manager_s i2c_manager = {(sizeof(i2cs)/sizeof(i2cs[0])), i2c_bus_init, i2c_work, I2CStop, I2C_IRQHandler, &i2c_listener};
+// const struct i2c_manager_s * const i2c_bus_manager = &i2c_manager;
 #endif
 
--- cpu/lpc17xx/i2c_lpc17xx.h
+++ cpu/lpc17xx/i2c_lpc17xx.h
@@ -8,42 +8,42 @@
  #define __I2C_LPC1764_H__
 
 #include "board_manager.h"
-	#if CPU_lpc17xx
-		#include "i2c_bus.h"
-		
-		#define I2CONSET_I2EN		0x00000040  /* I2C Control Set Register */ 
-		#define I2CONSET_AA			0x00000004 
-		#define I2CONSET_SI			0x00000008 
-		#define I2CONSET_STO		0x00000010 
-		#define I2CONSET_STA		0x00000020 
+    #if CPU_lpc17xx
+    #include "i2c_bus.h"
+
+    #define I2CONSET_I2EN    0x00000040  /* I2C Control Set Register */
+    #define I2CONSET_AA	     0x00000004
+    #define I2CONSET_SI	     0x00000008
+    #define I2CONSET_STO     0x00000010
+    #define I2CONSET_STA     0x00000020
 		 
-		#define I2CONCLR_AAC		0x00000004  /* I2C Control clear Register */ 
-		#define I2CONCLR_SIC		0x00000008 
-		#define I2CONCLR_STAC		0x00000020 
-		#define I2CONCLR_I2ENC		0x00000040 
-		 
-		#define I2DAT_I2C			0x00000000  /* I2C Data Reg */ 
-		#define I2ADR_I2C			0x00000000  /* I2C Slave Address Reg */ 
-		#define I2SCLH_SCLH			0x00000080  /* I2C SCL Duty Cycle High Reg */ 
-		#define I2SCLL_SCLL			0x00000080  /* I2C SCL Duty Cycle Low Reg */ 
-		/*
-		 * I2C总线结构体描述
-		 * {地址，电源(设置寄存器PCONP)，{引脚功能选择}}
-		 */
-		 #define I2C_A {\
-						{LPC_I2C0, 7, {&(LPC_PINCON->PINSEL1),0x01400000}},\
-					 	{LPC_I2C1, 19, {&(LPC_PINCON->PINSEL0), 0xf}},\
-					  	{LPC_I2C2, 26, {&(LPC_PINCON->PINSEL0), 0xaa << 20}}\
-					}
-		 struct i2c_s{
-			LPC_I2C_TypeDef * const bus;
-			const unsigned int pconp;
-			const struct{
-				volatile uint32_t * const pin_p;
-				const unsigned int pin_v;
-			} pin;
-		};
-		#define i2c_count() (sizeof(i2cs)/sizeof(i2cs[0]))
-		extern const struct i2c_manager_s * const i2c_bus_manager;
-	#endif
+    #define I2CONCLR_AAC     0x00000004  /* I2C Control clear Register */
+    #define I2CONCLR_SIC     0x00000008
+    #define I2CONCLR_STAC    0x00000020
+    #define I2CONCLR_I2ENC   0x00000040
+
+    #define I2DAT_I2C        0x00000000  /* I2C Data Reg */
+    #define I2ADR_I2C        0x00000000  /* I2C Slave Address Reg */
+    #define I2SCLH_SCLH      0x00000080  /* I2C SCL Duty Cycle High Reg */
+    #define I2SCLL_SCLL      0x00000080  /* I2C SCL Duty Cycle Low Reg */
+    /*
+     * I2C总线结构体描述
+     * {地址，电源(设置寄存器PCONP)，{引脚功能选择}}
+     */
+    #define I2C_A {\
+         {{LPC_I2C0, 7, {&(LPC_PINCON->PINSEL1),0x01400000}}, {master}},\
+         {{LPC_I2C1, 19, {&(LPC_PINCON->PINSEL0), 0xf}}, {master}},\
+         {{LPC_I2C2, 26, {&(LPC_PINCON->PINSEL0), 0xaa << 20}}, {master}}\
+    }
+    struct i2c_s{
+        LPC_I2C_TypeDef * const bus;
+        const unsigned int pconp;
+        const struct{
+            volatile uint32_t * const pin_p;
+            const unsigned int pin_v;
+        } pin;
+    };
+    #define i2c_count() (sizeof(i2cs)/sizeof(i2cs[0]))
+    extern const struct i2c_manager_s * const i2c_bus_manager;
+    #endif
 #endif
