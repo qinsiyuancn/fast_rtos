4a0883e1aacd320980b5d96d276c4a98ff2f7fe2
qinsiyuancn <130231069@qq.com>
2024-05-29 21:10:37 +0800

    change driver manager to pointer
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- inode/driver_manager.c
+++ inode/driver_manager.c
@@ -1,92 +1,103 @@
 #include "driver_manager.h"
 #include "driver.h"
 
-static const struct driver list = DRIVER_LIST_INITER;
+static const struct driver * const list[] = DRIVER_LIST_INITER;
 
 unsigned int driver_open(unsigned int driver,  unsigned int dev)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].open)
-            return list[driver].open(dev);
+        if(list[driver])
+            if(list[driver]->open)
+                return list[driver]->open(dev);
     return -1u;
 }
 
 unsigned int driver_read(unsigned int driver, unsigned int dev, void *buffer, unsigned long size)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].open)
-            return list[driver].read(dev, buffer, size);
+        if(list[driver])
+            if(list[driver]->read)
+                return list[driver]->read(dev, buffer, size);
     return -1u;
 }
 
 unsigned int driver_write(unsigned int driver, unsigned int dev, const void * buffer, unsigned long size)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].write)
-            return list[driver].write(dev, buffer, size);
+        if(list[driver])
+            if(list[driver]->write)
+                return list[driver]->write(dev, buffer, size);
     return -1u;
 }
 
 unsigned int driver_ioctl(unsigned int driver, unsigned int dev, unsigned int request, unsigned long param)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].ioctl)
-            return list[driver].ioctl(dev, request, param);
+        if(list[driver])
+            if(list[driver]->ioctl)
+                return list[driver]->ioctl(dev, request, param);
     return -1u;
 }
 
 unsigned int driver_close(unsigned int driver, unsigned int dev)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].close)
-            return list[driver].close(dev);
+        if(list[driver])
+            if(list[driver]->close)
+                return list[driver]->close(dev);
     return -1u;
 }
 
 void driver_init(unsigned int driver)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].init)
-            list[driver].init();
+        if(list[driver])
+            if(list[driver]->init)
+                list[driver]->init();
 }
 
 unsigned int driver_start(unsigned int driver, unsigned int dev, const unsigned char * send_buffer, unsigned int send_size, unsigned char* recv_buffer, unsigned int recv_size)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].start)
-            return list[driver].start(dev, send_buffer, send_size, recv_buffer, recv_size);
+        if(list[driver])
+            if(list[driver]->start)
+                return list[driver]->start(dev, send_buffer, send_size, recv_buffer, recv_size);
     return -1u;
 }
 
 unsigned int driver_stop(unsigned int driver,unsigned int dev)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].stop)
-            return list[driver].stop(dev);
+        if(list[driver])
+            if(list[driver]->stop)
+                return list[driver]->stop(dev);
     return -1u;
 }
 
 unsigned char driver_getchar(unsigned int driver, unsigned int dev)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].getchar)
-            return list[driver].getchar(dev);
+        if(list[driver])
+            if(list[driver]->getchar)
+                return list[driver]->getchar(dev);
     return 0xff;
 }
 
 unsigned short driver_getshort(unsigned int driver, unsigned int dev)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].getshort)
-            return list[driver].getshort(dev);
+        if(list[driver])
+            if(list[driver]->getshort)
+                return list[driver]->getshort(dev);
     return 0xffff;
 }
 
 unsigned long driver_getlong(unsigned int driver, unsigned int dev)
 {
     if(driver < (sizeof(list)/sizeof(list[0])))
-        if(list[driver].getlong)
-            return list[driver].getlong(dev);
+        if(list[driver])
+            if(list[driver]->getlong)
+            return list[driver]->getlong(dev);
     return -1ul;
 }
 
