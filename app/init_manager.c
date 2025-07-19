711d114351f03078489269c4ee82c5c7c5fde51a
qinsiyuancn <130231069@qq.com>
2025-07-19 18:02:13 +0800

    add init manager
    Signed-off-by: qinsiyuancn <130231069@qq.com>

---
+++ app/init_manager.c
@@ -0,0 +1,13 @@
+#include "init_manager.h"
+
+typedef unsigned int (*const init_fun)();
+
+unsigned int init()
+{
+    extern init_fun INIT_FUNCTION_LIST;
+    unsigned int i = 0;
+    init_fun init_fun[] = {INIT_FUNCTION_LIST};
+    for(i = 0; i < (sizeof(init_fun)/sizeof(init_fun[])); i++)
+        init_fun[i]();
+    return 0;
+}
---
+++ app/init_manager.h
@@ -0,0 +1,6 @@
+#ifndef __INIT_MANAGER_H__
+#define __INIT_MANAGER_H__
+
+extern unsigned int init();
+
+#endif
