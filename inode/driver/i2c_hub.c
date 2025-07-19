e29f93e49082bcb468b44e9a44b85567c931798e
qinsiyuancn <130231069@qq.com>
2025-07-19 11:59:25 +0800

    fix i2c_hub
    Signed-off-by: qinsiyuancn <130231069@qq.com>

---
+++ inode/driver/i2c_hub.c
@@ -0,0 +1,20 @@
+/*
+ * i2c_hub.c
+ * create by qinsiyuan
+ * 			on 2016-7-13
+ */
+
+#include "driver.h"
+
+
+static unsigned int write(unsigned int dev, const unsigned char *buf, unsigned long count)
+{
+    if (buf && count) {
+        unsigned char b = *buf | TEMPERATURE_SELECTOR_ENABLE_BIT;
+        return device_send(dev, b, 1);
+    }
+    return 0;
+}
+
+static const struct driver driver = {"i2c_hub", {NULL, NULL, NULL, write}};
+
