f9e34d77d24b617cc21ff4be3bce6679afe94940
qinsiyuancn <130231069@qq.com>
2025-07-20 11:00:14 +0800

    wrong string copy api
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- app/service/string/service_string_remote.c
+++ app/service/string/service_string_remote.c
@@ -29,7 +29,7 @@ unsigned int service_remote_power_off(const char *msg, char * ret, unsigned int 
         order = strstr(msg, "poweroff slot");
         if(order){
             if(1 == sscanf(order, "poweroff slot:0x%hhx\r", &slot)){
-                strcpy(ret, remote_power_off(slot)? "ok\r" : "request error\r", size);
+                strncpy(ret, remote_power_off(slot)? "ok\r" : "request error\r", size);
             }
             return 1;
         }
