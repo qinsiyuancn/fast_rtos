b674bf3bc96a07c99121e30a7060502ec48f2873
qinsiyuancn <130231069@qq.com>
2025-07-20 10:52:04 +0800

    controller
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- task/tasks/controller.c
+++ task/tasks/controller.c
@@ -8,17 +8,17 @@ static task_buffer stack[0x512];
  */
 static  void  controller (void *p_arg)
 {
-	int fd = 0;
-	char buffer [0x40] = {0};
-	OS_CPU_SysTickInit();
-	fd = open("ttyS",0);
-	ioctl(fd, 3, (char *)'\r');
-	while(1){
-		read(fd, buffer, sizeof(buffer)/sizeof(buffer[0]));
-		request_service_duty(buffer);
-		write(fd,buffer,strlen(buffer));
-		OSTimeDlyHMSM(0, 0, 0, 20);
-	}
+    int fd = 0;
+    char buffer [0x40] = {0};
+    OS_CPU_SysTickInit();
+    fd = open("ttyS",0);
+    ioctl(fd, 3, (char *)'\r');
+    while(1){
+        read(fd, buffer, sizeof(buffer)/sizeof(buffer[0]));
+        request_service_duty(buffer, buffer, sizeof(buffer)/sizeof(buffer[0]));
+        write(fd,buffer,strlen(buffer));
+        OSTimeDlyHMSM(0, 0, 0, 20);
+    }
 }
 
 static const struct Task task = {fun, define_stack(stack), 4};
