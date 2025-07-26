b65850bdce1f702d116e41fb2c38dba9a3f703e2
qinsiyuancn <130231069@qq.com>
2025-07-26 08:14:54 +0800

    fix bus define
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- bus/bus.h
+++ bus/bus.h
@@ -1,17 +1,17 @@
 #ifndef __BUS_H__
 #define __BUS_H__
 
-struct stream
+struct bus_stream
 {
-    unsigned int (*const read_start)(unsigned int dev, const unsigned char * send_data, unsigned int send_size, unsigned char * recv_data, unsigned int recv_size);
-    unsigned int (*const read_stop) (unsigned int dev);
+    unsigned int (*const start)(unsigned int dev, const unsigned char * send_data, unsigned int send_size, unsigned char * recv_data, unsigned int recv_size);
+    unsigned int (*const stop) (unsigned int dev);
     unsigned char (* const getchar)(unsigned int dev);
     unsigned short (* const getshort)(unsigned int dev);
     unsigned long (* const getlong)(unsigned int dev);
 };
 
 
-struct buffer_bus
+struct bus_buffer
 {
     unsigned int (* const send)(unsigned int dev, const unsigned char * data, unsigned int size);
     unsigned int (* const recv)(unsigned int dev, unsigned char * data, unsigned int size);
@@ -21,8 +21,8 @@ struct buffer_bus
 struct bus
 {
     const char * const name;
-    const struct stream_bus stream;
-    const struct buffer_bus buffer;
+    const struct bus_stream stream;
+    const struct bus_buffer buffer;
 };
 
 typedef const struct bus * const bus_pointer;
--- hal/eeprom.c
+++ hal/eeprom.c
@@ -1,48 +1,62 @@
 /*
- * service_eeprom.c
+ * eeprom.c
  * create by qinsiyuan
  *			on 2016-11-8
- * 提供eeprom读写的服务
  */
 
- #include "service_eeprom.h"
-static const struct clock delay = {0,0,0,100};
+#include "eeprom.h"
+
+static const char * const name[] = EEPROM_NAME;
+
+unsigned int eeprom_read(unsigned int dev, unsigned short address, unsigned char * data, unsigned int size)
+{
+    if(data && size)
+        return request_file(name[dev], (unsigned char *)&address, 2, data, size);
+    return 0;
+}
+
+unsigned int eeprom_write(unsigned int dev, unsigned short address, const unsigned char *data, unsigned int size)
+{
+    if(data && size)
+        return request_file(dev, (unsigned char *)&address, 2, data, size);
+    return 1;
+}
+
 
 int service_eeprom_read(char *msg)
 {
-	unsigned short address[1] = {0};
-	const char * order = strstr(msg,"read ipmc eeprom ");
-	if(order){
-		if(1 == sscanf(order, "read ipmc eeprom addr:0x%hx\r", address)){
-			*(unsigned short *)msg = *address;
-			msg[request("eeprom", msg, 2, msg, 1, &delay)]='\0';
-			sprintf(msg, "%03d\r", msg[0]);			
-		}
-		return 1;
-	}
-	return 0;
+    unsigned short address[1] = {0};
+    const char * order = strstr(msg,"read ipmc eeprom ");
+    if(order){
+        if(1 == sscanf(order, "read ipmc eeprom addr:0x%hx\r", address)){
+            *(unsigned short *)msg = *address;
+            sprintf(msg, "%03d\r", msg[0]);			
+        }
+     return 1;
+    }
+    return 0;
 }
 
 int service_eeprom_write(char *msg)
 {
-	unsigned short address = 0;
-	const char * order = strstr(msg,"write ipmc eeprom ");
-	if(order){
-		if(1 == sscanf(order, "write ipmc eeprom addr:0x%hx value:", &address)){
-			*(1 + strstr(order, "\r")) = 0;
-			order = strstr(order, "e:");
-			*(unsigned short *)order = address;
-			address = 2 + strlen(order + 2);
-			if(address == setdata("eeprom", order, address)){
-				//strcpy(msg, "ok!\r");
-				sprintf(msg, "%03d\r", order[2]);
-			}else{
-				//strcpy(msg, "set data error\r");
-				sprintf(msg, "%03d\r", order[2]);
-			}
-		}
-		return 1;
-	}
-	return 0;
+    unsigned short address = 0;
+    const char * order = strstr(msg,"write ipmc eeprom ");
+    if(order){
+        if(1 == sscanf(order, "write ipmc eeprom addr:0x%hx value:", &address)){
+            *(1 + strstr(order, "\r")) = 0;
+            order = strstr(order, "e:");
+            *(unsigned short *)order = address;
+            address = 2 + strlen(order + 2);
+            if(address == setdata("eeprom", order, address)){
+                //strcpy(msg, "ok!\r");
+                sprintf(msg, "%03d\r", order[2]);
+            }else{
+                //strcpy(msg, "set data error\r");
+                sprintf(msg, "%03d\r", order[2]);
+            }
+        }
+        return 1;
+    }
+    return 0;
 }
 
