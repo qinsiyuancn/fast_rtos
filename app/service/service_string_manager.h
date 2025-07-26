e1f0f749f101ca7921c0fcd285e069fff93dab83
qinsiyuancn <130231069@qq.com>
2025-07-26 08:41:02 +0800

    fix service string mananger format
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- app/service/service_string_manager.h
+++ app/service/service_string_manager.h
@@ -7,7 +7,6 @@
 #ifndef __SERVICE_STRING_MANAGER_H__
 #define __SERVICE_STRING_MANAGER_H__
 
-					 
 /*
  *
  */
@@ -16,6 +15,6 @@ extern unsigned int request_service(unsigned long fd, const char *msg, char* ret
 /*
  *
  */
-extern unsigned int request_service_duty(char * msg, char * ret, unsigned int size);
+extern unsigned int request_service_duty(const char * msg, char * ret, unsigned int size);
 
 #endif
