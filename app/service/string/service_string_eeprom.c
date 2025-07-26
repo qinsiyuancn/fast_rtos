f5bf92e623072190530b1985ae12e7dd2176c354
qinsiyuancn <130231069@qq.com>
2025-07-26 11:48:59 +0800

    fix service format of eeprom
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- app/service/string/service_string_eeprom.c
+++ app/service/string/service_string_eeprom.c
@@ -7,26 +7,29 @@
 #define ORDER_EEPROM_READ EEPROM READ
 #define ORDER_EEPROM_WRITE EEPROM WRITE
 
-unsigned int service_eeprom_read(const char *msg, char * ret, unsigned int size)
+unsigned int service_eeprom_read(const char *msg, unsigned int fd)
 {
-    unsigned short address = {0};
+    char name[10];
+    unsigned short address = 0;
     unsigned int read_size = 0;
     const char * order = strstr(msg, ORDER_EEPROM_READ);
     if(order){
-        if(1 == sscanf(order, ORDER_EEPROM_READ " addr:0x%hx size:%u\r", &address, &read_size))
-            eeprom_read(0, address, ret, read_size);
+        if(1 == sscanf(order, ORDER_EEPROM_READ "name:%s addr:0x%hx size:%u\r", name, &address, &read_size))
+            write(fd, ret, eeprom_read(name, address, ret, read_size));
         return 1;
     }
     return 0;
 }
 
-unsigned int service_eeprom_write(const char *msg, const char * ret, unsigned int size)
+unsigned int service_eeprom_write(const char *msg, unsigned int fd)
 {
     unsigned short address = 0;
+    const char * ret = NULL;
     const char * order = strstr(msg, ORDER_EEPROM_WRITE);
     if(order){
-        if(1 == sscanf(order, ORDER_EEPROM_WRITE " addr:0x%hx", &address)){
-            strcpy(ret, eeprom_write(0, address, ret) ? "ok\r" : "error\r");
+        if(1 == sscanf(order, ORDER_EEPROM_WRITE "name:%s addr:0x%hx", name, &address)){
+            ret = eeprom_write(name, address, strstr(order, "value:") + 6) ? "ok\r" : "error\r";
+            write(fd, ret, strlen(ret));
         }
         return 1;
     }
--- hal/eeprom.c
+++ hal/eeprom.c
@@ -6,19 +6,17 @@
 
 #include "eeprom.h"
 
-static const char * const name[] = EEPROM_NAME;
-
-unsigned int eeprom_read(unsigned int dev, unsigned short address, char * data, unsigned int size)
+unsigned int eeprom_read(const char * name, unsigned short address, char * data, unsigned int size)
 {
     if(data && size)
-        return request_file(name[dev], (unsigned char *)&address, 2, (unsigned char *)data, size);
+        return request_file(name, (unsigned char *)&address, 2, (unsigned char *)data, size);
     return 0;
 }
 
-unsigned int eeprom_write(unsigned int dev, unsigned short address, const char *data, unsigned int size)
+unsigned int eeprom_write(const char *name, unsigned short address, const char *data, unsigned int size)
 {
     if(data && size)
-        return request_file(name[dev], (unsigned char *)&address, 2, (const unsigned char *)data, size);
+        return request_file(name, (unsigned char *)&address, 2, (const unsigned char *)data, size);
     return 0;
 }
 
