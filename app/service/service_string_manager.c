d9f76bf0041cdfa6cb2ac18c90d0485bd5223678
qinsiyuancn <130231069@qq.com>
2025-07-29 13:16:43 +0800

    fix request string service
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- app/service/service_string_manager.c
+++ app/service/service_string_manager.c
@@ -6,7 +6,7 @@
 
 #include "service_string_manager.h"
 
-typedef unsigned int (*out_service)(const char * msg, char * ret, unsigned int size);
+typedef unsigned int (*out_service)(const char *, unsigned int);
 
 extern out_service SERVICE_LIST;
 
@@ -15,27 +15,28 @@ static out_service const out_service_list[] = {SERVICE_LIST};
 /*
  *
  */
-unsigned int request_service(unsigned long fd, const char *msg, char* ret, unsigned int size)
+unsigned int request_service(unsigned long index, const char *msg, unsigned int fd)
 {
     if(fd < sizeof(out_service_list)/sizeof(out_service_list[0])){
-        if(out_service_list[fd])
-            return out_service_list[fd](msg, ret, size);
+        if(out_service_list[index])
+            return out_service_list[index](msg, fd);
     }
-    return 0; 
+    return 0;
 }
 
 /*
  *
  */
-unsigned int request_service_duty(char * msg, char * ret, unsigned int size)
+unsigned int request_service_duty(const char * msg, unsigned int fd)
 {
+    static const char * const error = "no that service!\r";
     unsigned long i = 0;
     for(i = 0; i < sizeof(out_service_list)/sizeof(out_service_list[0]); i++){
-        if(request_service(i, msg, size)){
+        if(request_service(i, fd)){
             return 1;
         }
     }
-    strncpy(ret, "no that service!\r", size);
+    write(fd, error, strlen(error));
     return 0;
 }
 
--- app/service/service_string_manager.h
+++ app/service/service_string_manager.h
@@ -10,11 +10,11 @@
 /*
  *
  */
-extern unsigned int request_service(unsigned long fd, const char *msg, char* ret, unsigned int size);
+extern unsigned int request_service(unsigned long index, const char *msg, unsigned int fd);
 
 /*
  *
  */
-extern unsigned int request_service_duty(const char * msg, char * ret, unsigned int size);
+extern unsigned int request_service_duty(const char * msg, unsigned int fd);
 
 #endif
