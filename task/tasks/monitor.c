de03fedd7dc5ab098f509961454743f93660c206
qinsiyuancn <130231069@qq.com>
2025-07-30 16:52:38 +0800

    fix task monitor
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- task/tasks/monitor.c
+++ task/tasks/monitor.c
@@ -1,30 +1,18 @@
 #include "monitor.h"
+#include "task.h"
+#include "service_inner_manager.h"
 
 static stack_buffer stack[64]; 
 
 /*
- * ÂÖÑµ²Ù×÷
+ *
  */
 static void fun(void *p_arg)
 {
-	unsigned char count1 = 5;
-	OS_CPU_SysTickInit();
-	while(count1--) {
-		led_blue_on();
-		OSTimeDlyHMSM(0, 0, 0, 900);
-		led_blue_off();
-		OSTimeDlyHMSM(0, 0, 0, 100);
-	}
-	set_sys_dc_12v_en_on();
-	led_blue_off();
-	led_yellow_on();
-	OSTimeDlyHMSM(0, 0, 3, 0);		
-	while(1){
-		#if RTM_SUPPORT	
-			service_rtm_power();
-		#endif
-		OSTimeDlyHMSM(0, 0, 0, 20);
-	}
+    while(1){
+        request_string_service_duty();
+        OSTimeDlyHMSM(0, 0, 0, 20);
+    }
 }
 
 static const struct Task task = {fun, define_stack(stack), 5};
--- task/tasks/monitor.h
+++ task/tasks/monitor.h
@@ -1,7 +1,6 @@
 #ifndef __MONITOR_H__
 #define __MONITOR_H__
 
-    #include "task.h"
-		
-		extern const struct task * const monitor;
+extern const struct task * const monitor;
+
 #endif
