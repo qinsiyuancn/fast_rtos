cd91a20ee5fe4de0dd3e629e80a3f982a3c2d5d5
qinsiyuancn <130231069@qq.com>
2025-07-30 16:49:29 +0800

    fix service inner manager interface
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- service/inner/service_inner_manager.h
+++ service/inner/service_inner_manager.h
@@ -15,6 +15,6 @@ extern unsigned int request_inner_service(unsigned long index);
 /*
  *
  */
-extern unsigned int request_string_service_duty(const char * msg, unsigned int fd);
+extern unsigned int request_string_service_duty();
 
 #endif
