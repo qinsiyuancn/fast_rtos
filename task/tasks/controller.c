21d0fd8d859d8008307c6c6a377435911bdf4e57
qinsiyuancn <130231069@qq.com>
2025-07-31 15:48:32 +0800

    fix task controller
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- task/tasks/controller.c
+++ task/tasks/controller.c
@@ -1,25 +1,27 @@
 #include "controller.h"
 #include "task.h"
+#include "os_adapter.h"
 
-static task_buffer stack[0x512]; 
+static task_buffer stack[STACK_CONTROLLER_SIZE]; 
 
 /*
  *
  */
 static  void  controller (void *p_arg)
 {
-    unsigned char buffer[40];
-    unsigned int i = 0;
+    static unsigned char buffer[BUFFER_SIZE];
+    static unsigned int i = 0;
     //OS_CPU_SysTickInit();
-    const unsigned int fd = open("ttyS",0);
+    static const unsigned int fd = open("ttyS",0);
     start(fd, NULL, 0, NULL, 0);
     while(1){
         i = 0;
         do{
             buffer[i] = getchar(fd);
-	}while(buffer[i++] != MESSAGE_END);
-        request_string_service_duty(buffer, fd);
-        OSTimeDlyHMSM(0, 0, 0, 20);
+	}while(buffer[i++] != MESSAGE_END && i != (sizeof(buffer)/sizeof(buffer[0])));
+	if(i < (sizeof(buffer)/sizeof(buffer[0])))
+            request_string_service_duty(buffer, fd);
+        delay(CONTROLLER_TIMER);
     }
 }
 
