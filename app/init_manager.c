652c1c14812d870651612db17c4dbb4b249ad9c9
qinsiyuancn <130231069@qq.com>
2026-02-26 01:36:09 +0800

    change init manager implement to use init
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- app/init_manager.c
+++ app/init_manager.c
@@ -1,6 +1,5 @@
 #include "init_manager.h"
-
-typedef unsigned int (*const init_fun)();
+#include "init.h"
 
 unsigned int init()
 {
@@ -12,9 +11,6 @@ unsigned int init()
     OSInit();
 */
     extern init_fun INIT_FUNCTION_LIST;
-    unsigned int i = 0;
-    init_fun init_fun[] = {INIT_FUNCTION_LIST};
-    for(i = 0; i < (sizeof(init_fun)/sizeof(init_fun[0])); i++)
-        init_fun[i]();
-    return 0;
+    const init_fun init_fun[] = {INIT_FUNCTION_LIST};
+    return do_init(init_fun, sizeof(init_fun)/sizeof(init_fun[0]));
 }
