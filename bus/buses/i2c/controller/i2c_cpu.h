5a9b3e6c40170dd94135b13ad4a2c5bea3ef260f
qinsiyuancn <130231069@qq.com>
2026-05-31 14:32:33 +0800

    delete cpu limit in i2c_cpu
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- bus/buses/i2c/controller/i2c_cpu.h
+++ bus/buses/i2c/controller/i2c_cpu.h
@@ -5,8 +5,6 @@
  * i2c与cpu相关驱动部分
  */
 
-#if CPU_lpc17xx
-
 extern unsigned int cpu_i2c_stop(unsigned char fd);
 
 extern unsigned int cpu_i2c_clear_sic(unsigned char fd);
@@ -16,6 +14,4 @@ extern unsigned int cpu_i2c_start(unsigned char fd);
 extern void I2C_IRQHandler(unsigned char fd);
 
 extern unsigned int cpu_i2c_init(unsigned char fd, enum i2c_mode mode);
-
-#endif
 
