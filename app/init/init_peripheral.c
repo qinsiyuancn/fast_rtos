d1bb941224a1d0e005523e84154280f1d27bf7d3
qinsiyuancn <130231069@qq.com>
2026-06-21 10:41:21 +0800

    fix init peripheral
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- app/init/init_peripheral.c
+++ app/init/init_peripheral.c
@@ -1,9 +1,9 @@
 #include "init.h"
+#include "file_manager.h"
 static unsigned int init()
 {
-    extern PERIPHERAL_INIT_LIST;
-    const init_fun init_fun[] = {PERIPHERAL_INIT_LIST};
-    return do_init(init_fun, sizeof(init_fun)/sizeof(init_fun[0]));
+    init_files();
+    return 0;
 }
 
 init_fun init_peripheral = init;
