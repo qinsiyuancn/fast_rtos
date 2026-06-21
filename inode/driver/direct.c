6052ab35be96cd7cd7cc423661f33eb6d0e3aaea
qinsiyuancn <130231069@qq.com>
2026-06-21 11:46:11 +0800

    fix driver direct
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- inode/driver/direct.c
+++ inode/driver/direct.c
@@ -1,8 +1,7 @@
 #include "driver.h"
-#include "driver_direct.h"
+#include "device_manager.h"
 
-
-static const struct driver driver = {"direct", NULL, {NULL, driver_read_direct, driver_write_direct}, 
-	                                       {driver_start_direct, NULL, driver_getchar_direct, driver_getshort_direct, driver_getlong_direct }};
+static const struct driver driver = {"direct", NULL, {NULL, driver_recv, device_send}, 
+	                                       {device_start, NULL, device_getchar, device_getshort, device_getlong }};
 
 driver_pointer direct = &driver;
